fa3aba7eb991fe0a74a139e7d6ebf1b1c5e16248
julien-ba <julienbarbier42@gmail.com>
2024-12-01 11:02:02 +0100

    renamed icon

--- my-sportsee-frontend/src/assets/icons/index.js
+++ my-sportsee-frontend/src/assets/icons/index.js
@@ -1,7 +1,7 @@
 import Biking from './biking.svg?react';
 import Sportsee from './sportsee.svg?react';
 import Swimming from './swimming.svg?react';
-import Weight from './weight.svg?react';
+import Weightlifting from './weightlifting.svg?react';
 import Yoga from './yoga.svg?react';
 
-export { Biking, Sportsee, Swimming, Weight, Yoga };
+export { Biking, Sportsee, Swimming, Weightlifting, Yoga };
